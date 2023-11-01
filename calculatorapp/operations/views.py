28fb2edc205f5cfc2ea988e8b0f66c3ffb191eaf
Afreen <afisreyas@gmail.com>
2023-11-01 15:50:17 +0530

    cal_app

--- calculatorapp/operations/views.py
+++ calculatorapp/operations/views.py
@@ -75,3 +75,34 @@ class FactorialView(View):
             fact=fact*i
         print(fact)
         return render(request,"fact.html",{"output":fact})
+    
+class LeapYearView(View):
+    def get(self,request,*args,**kwargs):
+        return render(request,"lp.html")
+    
+    def post(self,request,*args,**kwargs):
+        year=int(request.POST.get("year"))
+
+        if(year %100==0 and year %400==0 or year %100!=0 and year %4==0):
+            result=f"{year} is leap year"
+        else:
+            result=f"{year} is not leap year"
+
+            return render(request,"lp.html",{"out":result})
+        
+class BmiView(View):
+    def get(self,request,*args,**kwargs):
+        return render(request,"bmi.html")
+    def post(self,request,*args,**kwargs):
+        num1=int(request.POST.get("kg"))
+        num2=int(request.POST.get("cm"))
+
+        bmi=int(num1/(num2)**2)
+
+        return render(request,"bmi.html",{"out":bmi})
+
+    
+
+class IndexView(View):
+    def get(self,request,*args,**kwargs):
+        return render(request,"index.html")
