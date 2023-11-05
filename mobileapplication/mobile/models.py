bd07232751778168e144fdf3e169c9a8a6faf90c
Afreen <afisreyas@gmail.com>
2023-11-05 16:16:59 +0530

    djano_works

---
+++ mobileapplication/mobile/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Mobiles(models.Model):
+    name=models.CharField(max_length=40,unique=True)
+    price=models.PositiveIntegerField()
+    brand=models.CharField(max_length=40)
+    specs=models.CharField(max_length=200)
+    display=models.CharField(max_length=200)
